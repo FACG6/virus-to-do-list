8f29ec59930978d1fcbdf9641c2afc0e46d7e06a
Khader <kakastarcris@gmail.com>
2019-01-15 15:15:50 +0200

    modify test.js file

--- test.js
+++ test.js
@@ -1,7 +1,39 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
-  t.end();
+test('', function(t) {
+  const actual = logic.addTodo([
+    {id: 0, description: 'First One', done: false},
+    {id: 1, description: 'Second One', done: false},
+    {id: 2, description: 'Third One', done: false}
+  ], {id: 3, description: 'Forth One', done: false});
+  const expected = [
+    {id: 0, description: 'First One', done: false},
+    {id: 1, description: 'Second One', done: false},
+    {id: 2, description: 'Third One', done: false},
+    {id: 3, description: 'Forth One', done: false}
+  ];
+  t.deepEqual(actual, expected, 'The Item has added');
+  t.end();  
+});
+
+test('', function(t) {
+  const actual = logic.addTodo([], {id: 3, description: 'Forth One', done: false});
+  const expected = [{id: 3, description: 'Forth One', done: false}
+  ];
+  t.deepEqual(actual, expected, 'The Item has added');
+  t.end();  
+});
+
+test('', function(t) {
+  const actual = logic.addTodo([
+    {id: 0, description: 'First One', done: false},
+    {id: 2, description: 'Third One', done: false}
+  ], {id: 2, description: 'Third One', done: false});
+  const expected = [
+    {id: 0, description: 'First One', done: false},
+    {id: 2, description: 'Third One', done: false}
+  ];
+  t.deepEqual(actual, expected, 'This Todo is already exist');
+  t.end();  
 });
